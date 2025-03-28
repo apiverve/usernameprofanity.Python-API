303f2164f37ce68192cea0eaa89f92948dc5d3ef
APIVerve Team <hello@apiverve.com>
2025-03-28 18:03:18 -0500

    Bug Fixes

--- src/apiverve_usernameprofanity/setup.py
+++ src/apiverve_usernameprofanity/setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='apiverve_usernameprofanity',
-    version='1.1.4',
+    version='1.1.9',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'requests',
-        'setuptools'
-    ],
+    install_requires=install_requires,
     description='Username Profanity Checker is a simple tool for checking if a username is inappropriate or profane. It returns if the username is inappropriate or profane.',
     author='APIVerve',
     author_email='hello@apiverve.com',
